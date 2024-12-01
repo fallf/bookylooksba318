092823289d93cfd968d7494aa5ad2e97ab79a8ee
fatou fall <fatoufall95@gmail.com>
2024-12-01 14:10:45 -0500

    setting up to create user

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const { parse } = require("path");
 const PORT = 3000;
 
+const users = [];
 app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.render("index.ejs", { name: "Fatou" });
 });
@@ -10,10 +16,28 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
+
+app.post("/login", (req, res) => {});
+
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
-app.post("/register", (req, res) => {});
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch (error) {
+    console.error("Error during registration:", error);
+    res.redirect("/register");
+  }
+  console.log(users);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
