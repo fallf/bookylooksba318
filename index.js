4c044dcb58240b6931a43b8dfec4e49f73f65e95
fatou fall <fatoufall95@gmail.com>
2024-12-01 13:21:20 -0500

    setting up ejs login/register/mainpages

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
-
 const PORT = 3000;
+
+app.set("view-engine", "ejs");
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index.ejs", { name: "Fatou" });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
 });
 
 app.listen(PORT, () => {
