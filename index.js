6e3d47f7f88fa6b0d04b96a624bb20495ee3b33a
fatou fall <fatoufall95@gmail.com>
2024-12-01 23:09:10 -0500

    added styling, added add data folder

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
 const methodOverride = require("method-override");
-
+const fs = require("fs");
 const initializePassport = require("./routes/passport-config");
 initializePassport(
   passport,
